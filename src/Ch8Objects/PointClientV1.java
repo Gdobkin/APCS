53604f42c73e48e4cc3579108b68e3a530e1314d
gdobkin <gaby.dobkin@gmail.com>
2018-11-27 13:38:07 -0500

    learning

--- src/Ch8Objects/PointClientV1.java
+++ src/Ch8Objects/PointClientV1.java
@@ -4,8 +4,21 @@ public class PointClientV1 {
     public static void main(String[] args) {
         PointV1 p1 = new PointV1();
         System.out.println("P1: ("+p1.x+", "+p1.y+")");
-        p1.x = 5;
-        p1.y = 7;
-        System.out.println("P1: ("+p1.x+", "+p1.y+")");
+        PointV1 p2 = new PointV1(5,5);
+        //p2.x=7;
+        //p2.y=8;
+        p1.translate(5,4);
+        System.out.println("P1': ("+p1.x+", "+p1.y+")");
+
+        p1.translateV1(3,2);
+        System.out.println("P1'': ("+p1.x+", "+p1.y+")");
+
+        p1.setLocation(7,6);
+        System.out.println("P1''': ("+p1.x+", "+p1.y+")");
+
+        p1.setLocationV1(0,0);
+        System.out.println("P1'''': ("+p1.x+", "+p1.y+")");
+
     }
 }
+/* instantiate */
--- src/Ch8Objects/PointV1.java
+++ src/Ch8Objects/PointV1.java
@@ -1,14 +1,57 @@
 package Ch8Objects;
 
+import java.awt.*;
+
 public class PointV1 {
     //state
     public int x;
     public int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     //behavior
-    public void translate(int x, int y) {
+    public void translateV1(int dx, int dy){
+        x = x + dx;
+        y = y + dy;
+    }
+    public void translate(int dx, int dy) {
+        setX(getX()+dx);
+        setY(getY()+dy);
+    }
+    public void setLocationV1(int newX, int newY){
+        x = newX;
+        y = newY;
+    }
+    public void setLocation(int newX, int newY){
+        setX(newX);
+        setY(newY);
+    }
+    //distance and distanceFrom
+    public double distance(PointV1 other){
+        return(Math.sqrt(Math.pow(this.x-other.x,2)+(Math.pow(this.y-other.y,2))));
+    }
+    public static double distanceFromTwo(PointV1 first, PointV1 second){
+        return(Math.sqrt(Math.pow(first.x-second.x,2)+(Math.pow(first.y-second.y,2))));
+    }
+    //constructors
+    public PointV1(){
 
     }
-   // public PointV1(int x, int y){
+    public PointV1(int x, int y){
 
-    //}
+    }
 }
