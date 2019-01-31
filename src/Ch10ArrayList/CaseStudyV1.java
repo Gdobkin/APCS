d3c5ad2a660fd46a12af183d207e069e1c7172b6
gdobkin <gaby.dobkin@gmail.com>
2019-02-01 06:38:14 +1200

    case study

--- src/Ch10ArrayList/CaseStudyV1.java
+++ src/Ch10ArrayList/CaseStudyV1.java
@@ -3,6 +3,7 @@ package Ch10ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;//the wheels on the bus go round and round, round and round, round and round
+import java.util.Collections;
 import java.util.Scanner;
 
 //the wheels on the bus go round and round, round and round, round and round
@@ -10,19 +11,77 @@ public class CaseStudyV1 {//the wheels on the bus go round and round, round and 
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner input1 = new Scanner(new File("text1.txt"));
-        Scanner input2 = new Scanner(new File("text1.txt"));
-        ArrayList<String> words1 = getWords(input1);
-        ArrayList<String> words2 = getWords(input2);
+        Scanner input2 = new Scanner(new File("text2.txt"));
+        ArrayList<String> words1 = wordListNP(input1);
+        ArrayList<String> words2 = wordListNP(input2);
+        ArrayList<String> overlap = overlap(words1,words2);
+        System.out.println(words1);
+        System.out.println(words2);
+        System.out.println(overlap);
 
+  }
 
-    }
-    public static ArrayList<String> getWords(Scanner input){
-        ArrayList<String> words1 = new ArrayList<>();
+    public static ArrayList<String> wordList(Scanner input){
+        //temp array
+        ArrayList<String> words = new ArrayList<>();
+        //populate
         while(input.hasNext()){
-            words1.add(input.next());
+            words.add(input.next().toLowerCase());
         }
-        return words1;
+        //sort
+        Collections.sort(words);
+        //get rid of duplicates by creating a new array where we will add unique words
+        ArrayList<String> unique = new ArrayList<>();
+        unique.add(words.get(0));
+        for(int i = 1; i<words.size(); i++){
+            if(!words.get(i).equals(words.get(i-1))) {
+                unique.add(words.get(i));
+            }
+        }
+        //return unique into other arrayList
+        return unique;
     }
 
-
+    public static ArrayList<String> wordListNP(Scanner input){
+        //temp array
+        ArrayList<String> words = new ArrayList<>();
+        //populate and get rid of punctuation
+        while(input.hasNext()){
+            String Text = input.next().toLowerCase();
+            String[] Res = Text.split("[\\p{Punct}\\s]+");
+            for (String s:Res){
+                words.add(s);
+            }
+        }
+        //sort
+        Collections.sort(words);
+        //get rid of duplicates by creating a new array where we will add unique words
+        ArrayList<String> unique = new ArrayList<>();
+        unique.add(words.get(0));
+        for(int i = 1; i<words.size(); i++){
+            if(!words.get(i).equals(words.get(i-1))) {
+                unique.add(words.get(i));
+            }
+        }
+        //return unique into other arrayList
+        return unique;
+    }
+    public static ArrayList<String> overlap(ArrayList<String> a, ArrayList<String> b){
+        int i1 = 0;
+        int i2 = 0;
+        ArrayList<String> common = new ArrayList<>();
+        while(i1<a.size()&&i2<b.size()){
+            int compare = a.get(i1).compareTo(b.get(i2));
+            if(a.get(i1).equals(b.get(i2))){
+                common.add(a.get(i1));
+                i1++;
+                i2++;
+            } else if(compare<1){
+                i1++;
+            } else{
+                i2++;
+            }
+        }
+        return common;
+    }
 }
