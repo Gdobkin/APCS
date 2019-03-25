e60915fc358572f2ca7a1344119ca4f1452bccaa
gdobkin <gaby.dobkin@gmail.com>
2019-03-25 10:38:07 -0700

    stock project

--- src/Ch9Inheritance/StockProject/Cash.java
+++ src/Ch9Inheritance/StockProject/Cash.java
@@ -2,10 +2,17 @@ package Ch9Inheritance.StockProject;
 
 public class Cash implements Asset {
     private double amount;
+    public Cash(double amount){
+        this.amount = amount;
+    }
     public double getMarketValue(){
-
+        return amount;
     }
     public double getProfit(){
+        return 0.0;
+    }
 
+    public void setAmount (double amount){
+        this.amount = amount;
     }
 }
--- src/Ch9Inheritance/StockProject/MutualFund.java
+++ src/Ch9Inheritance/StockProject/MutualFund.java
@@ -1,4 +1,22 @@
 package Ch9Inheritance.StockProject;
 
 public class MutualFund extends ShareAsset {
+    private double totalShares;
+
+    public MutualFund(String symbol, double currentPrice){
+        super(symbol,currentPrice);
+        totalShares = 0;
+    }
+
+    public double getMarketValue(){
+        return totalShares * getCurrentPrice();
+    }
+
+    public double getTotalShares(){
+        return totalShares;
+    }
+    public void purchase(int shares, double pricePerShare){
+        totalShares+=shares;
+        addCost(shares*pricePerShare);
+    }
 }
