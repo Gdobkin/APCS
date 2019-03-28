d70138c3adf6a90e7fcc24e218f1683421508f27
gdobkin <gaby.dobkin@gmail.com>
2019-03-28 10:22:54 -0700

    recursion

---
+++ src/Ch12Recursion/Practice.java
@@ -0,0 +1,32 @@
+package Ch12Recursion;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println(g(10,2));
+    }
+    public static int f(int x) {
+        if (x <= 4) {
+            return x + 4;
+        } else if (x > 4 && x <= 6) {
+            return f(x + 2) + 1;
+        } else {
+            return 2 * f(x - 3) - 3;
+        }
+    }
+
+    public static int g(int x, int y){
+        if(x<y){
+            return x*y;
+        } else if(x==y){
+            return g(x+1,y-1)-1;
+        } else {
+            return g(x-2,y+2)+2;
+        }
+    }
+
+    public static int h(int x){
+        if(x<10){
+            return x-4;
+        }
+    }
+}
